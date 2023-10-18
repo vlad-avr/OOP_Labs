5d70067571e8c85350c86a8842a51f078bd7e682
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-10-18 18:18:19 +0300

    implemented attributes and subclasses for Flower

--- Lab2/demo/src/main/java/com/example/flowers/Flower.java
+++ Lab2/demo/src/main/java/com/example/flowers/Flower.java
@@ -1,11 +1,52 @@
 package com.example.flowers;
 
 public class Flower {
-    public enum Soil{
+    public static enum Soil {
         podzol,
         turf_podzol,
         regular,
     }
+
+    public static enum Reproduction {
+        leaves,
+        turf_podzol,
+        regular,
+    }
+
     public String id;
-    
+    public String name;
+    public Soil soil;
+    public String origin;
+    public Visuals visuals = new Visuals();
+    public Tips tips = new Tips();
+    public Reproduction reproduction;
+
+    private class Visuals {
+        public int stalk_length;
+        public String color;
+        public int flower_diameter;
+
+        public String toString() {
+            return "\n\tAverage stalk length (cm) : " + stalk_length + "\n\tColor : " + color
+                    + "\n\tAverage flower diameter (cm) : " + flower_diameter;
+        }
+    }
+
+    private class Tips {
+        public int temp;
+        public int light;
+        public int water;
+
+        public String toString() {
+            return "\n\tIdeal average temperature : " + temp + "\n\tHours of illumination needed per day : " + light
+                    + "\n\tDaily needs in water (ml) : " + water;
+        }
+    }
+
+    public String toString() {
+        return "\nID : " + id + "\nName : " + name + "\nOrigin : " + origin + "\nSoil type + " + soil.toString()
+                + "\nVisual qualities : " + visuals.toString() + "\nGardening tips : " + tips.toString()
+                + "\nReproduces with " + reproduction.toString();
+    }
+
 }
