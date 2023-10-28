c84fe5c316b377acb6e49d999f835ef72137cd74
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-10-28 12:04:02 +0300

    implemented basic functions | need to implement more
    Will add getters/setters later

--- Lab2/demo/src/main/java/com/example/parser/MySAXParser.java
+++ Lab2/demo/src/main/java/com/example/parser/MySAXParser.java
@@ -3,6 +3,7 @@ package com.example.parser;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,6 +12,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
+import org.xml.sax.Attributes;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
@@ -35,10 +37,41 @@ public class MySAXParser {
     }
 
     class SchemaElement {
+        private String name;
+        private String type;
+        private List<SchemaElement> children;
+        private Map<String, String> attributes;
+
+        public void setName(String value) {
+            this.name = value;
+        }
+
+        public void setType(String value) {
+            this.type = value;
+        }
 
     }
 
     class SchemaComplexType {
+        private String name;
+        private List<SchemaElement> children;
+        private Map<String, String> attributes = new HashMap<>();
+
+        public void setName(String value) {
+            this.name = value;
+        }
+
+        public void addAttribute(String key, String value) {
+            attributes.put(key, value);
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setChildren(List<SchemaElement> currentSequence) {
+            this.children = currentSequence;
+        }
 
     }
 
@@ -52,5 +85,95 @@ public class MySAXParser {
         private Map<String, String> simpleTypes = new HashMap<>();
         private Map<String, SchemaComplexType> complexTypes = new HashMap<>();
         private SchemaElement rootElement;
+
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
+
+            if (qName.equals("xs:simpleType")) {
+                currentSimpleTypeName = atts.getValue("name");
+            }
+            if (qName.equals("xs:restriction")) {
+                currentSimpleTypeBaseType = atts.getValue("base");
+            }
+
+            if (qName.equals("xs:complexType")) {
+                currentComplexType = new SchemaComplexType();
+                currentComplexType.setName(atts.getValue("name"));
+            }
+
+            if (qName.equals("xs:sequence")) {
+                currentSequence = new ArrayList<>();
+            }
+
+            if (qName.equals("xs:element")) {
+                currentElement = new SchemaElement();
+                currentElement.setName(atts.getValue("name"));
+                currentElement.setType(atts.getValue("type"));
+                if (currentSequence != null) {
+                    currentSequence.add(currentElement);
+                } else {
+                    rootElement = currentElement;
+                }
+            }
+
+            if (qName.equals("xs:attribute")) {
+                currentComplexType.addAttribute(atts.getValue("name"), atts.getValue("type"));
+            }
+        }
+
+        @Override
+        public void endElement(String uri, String localName, String qName) throws SAXException {
+            if (qName.equals("xs:simpleType")) {
+                simpleTypes.put(currentSimpleTypeName, currentSimpleTypeBaseType);
+                currentSimpleTypeName = null;
+                currentSimpleTypeBaseType = null;
+            }
+            if (qName.equals("xs:complexType")) {
+                complexTypes.put(currentComplexType.getName(), currentComplexType);
+                currentComplexType = null;
+            }
+            if (qName.equals("xs:sequence")) {
+                if (currentComplexType != null) {
+                    currentComplexType.setChildren(currentSequence);
+                }
+                currentSequence = null;
+            }
+        }
+
+        @Override
+        public void endDocument() throws SAXException {
+            makeTree(rootElement);
+            printTree(rootElement, "");
+        }
+
+        public void makeTree(SchemaElement element) {
+            SchemaComplexType type = complexTypes.get(element.getType());
+            if (type != null) {
+                List<SchemaElement> children = type.getChildren();
+                element.setChildren(children);
+                for (SchemaElement child : children) {
+                    makeTree(child);
+                }
+                element.setAttributes(type.getAttributes());
+            } else {
+                element.setType(simpleTypes.get(element.getType()));
+            }
+        }
+    
+        private void printTree(SchemaElement element, String indent) {
+            System.out.println(indent + element.getName() + " : " + element.getType());
+            Map<String, String> attributes = element.getAttributes();
+            if (attributes != null) {
+                for (Map.Entry<String, String> entry : attributes.entrySet()) {
+                    System.out.println("    @" + entry.getKey() + " : " + simpleTypes.get(entry.getValue()));
+                }
+            }
+            List<SchemaElement> children = element.getChildren();
+            if (children != null) {
+                for (SchemaElement child : children) {
+                    printTree(child, indent + "    ");
+                }
+            }
+        }
     }
 }
