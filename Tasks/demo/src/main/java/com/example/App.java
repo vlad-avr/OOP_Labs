5377a234fd81ddcc13f465dd802bf4ad09e7dd4f
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-11-06 14:59:31 +0200

    made some djustments - learning reflection

--- Tasks/demo/src/main/java/com/example/App.java
+++ Tasks/demo/src/main/java/com/example/App.java
@@ -1,17 +1,14 @@
 package com.example;
 
-/**
- * Hello world!
- */
+import java.util.concurrent.Semaphore;
+
+import com.example.Reflection_task4.MyReflector;
+
 public final class App {
     private App() {
     }
-
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        MyReflector reflector = new MyReflector();
+        reflector.reflectClass(Thread.class);
     }
 }
--- Tasks/demo/src/main/java/com/example/Reflection_task4/MyReflector.java
+++ Tasks/demo/src/main/java/com/example/Reflection_task4/MyReflector.java
@@ -1,5 +1,44 @@
 package com.example.Reflection_task4;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.List;
+import java.lang.annotation.Annotation;
+
 public class MyReflector {
-    
+
+    public void reflectClass(Class<?> c) {
+        System.out.println(c.getName() + " class : ");
+        System.out.println("\n Public methods : \n");
+        List<Method> publicMethods = Arrays.asList(c.getMethods());
+        for (Method method : publicMethods) {
+            String toPrint = method.getName() + "(";
+            Class<?>[] paramTypes = method.getParameterTypes();
+            Parameter[] params = method.getParameters();
+            int N = method.getParameterCount();
+            // Annotation[][] params = method.getParameterAnnotations();
+            for (int i = 0; i < N; i++) {
+                toPrint += paramTypes[i].getName() + " " + params[i].getName() + ", ";
+            }
+            toPrint += ") returns " + method.getReturnType().getName();
+            System.out.println(toPrint);
+        }
+        System.out.println("\n Private methods : \n");
+        List<Method> allMethods = Arrays.asList(c.getDeclaredMethods());
+        for (Method method : allMethods) {
+            if (!publicMethods.contains(method)) {
+                String toPrint = method.getName() + "(";
+                Class<?>[] paramTypes = method.getParameterTypes();
+                Parameter[] params = method.getParameters();
+                int N = method.getParameterCount();
+                // Annotation[][] params = method.getParameterAnnotations();
+                for (int i = 0; i < N; i++) {
+                    toPrint += paramTypes[i].getName() + " " + params[i].getName() + ", ";
+                }
+                toPrint += ") returns " + method.getReturnType().getName();
+                System.out.println(toPrint);
+            }
+        }
+    }
 }
