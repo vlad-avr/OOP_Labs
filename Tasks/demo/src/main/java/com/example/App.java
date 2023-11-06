c81c500f964d6d2e448db824f4f06e8bebd0646a
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-11-06 18:38:34 +0200

    reflected String class

--- Tasks/demo/src/main/java/com/example/App.java
+++ Tasks/demo/src/main/java/com/example/App.java
@@ -7,6 +7,6 @@ public final class App {
     }
     public static void main(String[] args) {
         MyReflector reflector = new MyReflector();
-        reflector.reflectClass(MyReflector.class);
+        reflector.reflectClass(String.class);
     }
 }
