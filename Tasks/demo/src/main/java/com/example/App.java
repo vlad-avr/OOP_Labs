2298cde3907743da420e80e6f93a35ba1d7bc92c
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-11-06 15:24:04 +0200

    added fields reflector

--- Tasks/demo/src/main/java/com/example/App.java
+++ Tasks/demo/src/main/java/com/example/App.java
@@ -1,7 +1,5 @@
 package com.example;
 
-import java.util.concurrent.Semaphore;
-
 import com.example.Reflection_task4.MyReflector;
 
 public final class App {
@@ -9,6 +7,6 @@ public final class App {
     }
     public static void main(String[] args) {
         MyReflector reflector = new MyReflector();
-        reflector.reflectClass(Thread.class);
+        reflector.reflectClass(MyReflector.class);
     }
 }
--- Tasks/demo/src/main/java/com/example/Reflection_task4/MyReflector.java
+++ Tasks/demo/src/main/java/com/example/Reflection_task4/MyReflector.java
@@ -1,15 +1,37 @@
 package com.example.Reflection_task4;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.Arrays;
 import java.util.List;
-import java.lang.annotation.Annotation;
 
 public class MyReflector {
 
     public void reflectClass(Class<?> c) {
         System.out.println(c.getName() + " class : ");
+        getFields(c);
+        getMethods(c);
+    }
+
+    private void getFields(Class<?> c){
+        System.out.println("\n Public Fields : \n");
+        List<Field> publicFields = Arrays.asList(c.getFields());
+        for (Field field : publicFields) {
+            String toPrint = field.getName() + " " + field.getType().getName();
+            System.out.println(toPrint);
+        }
+        System.out.println("\n Private methods : \n");
+        List<Field> allFields = Arrays.asList(c.getDeclaredFields());
+        for (Field field : allFields) {
+            if (!publicFields.contains(field)) {
+                String toPrint = field.getName() + " " + field.getType().getName();
+                System.out.println(toPrint);
+            }
+        }
+    }
+
+    private void getMethods(Class<?> c){
         System.out.println("\n Public methods : \n");
         List<Method> publicMethods = Arrays.asList(c.getMethods());
         for (Method method : publicMethods) {
@@ -17,9 +39,11 @@ public class MyReflector {
             Class<?>[] paramTypes = method.getParameterTypes();
             Parameter[] params = method.getParameters();
             int N = method.getParameterCount();
-            // Annotation[][] params = method.getParameterAnnotations();
             for (int i = 0; i < N; i++) {
-                toPrint += paramTypes[i].getName() + " " + params[i].getName() + ", ";
+                toPrint += paramTypes[i].getName() + " " + params[i].getName();
+                if (i != N - 1) {
+                    toPrint += ", ";
+                }
             }
             toPrint += ") returns " + method.getReturnType().getName();
             System.out.println(toPrint);
@@ -32,9 +56,11 @@ public class MyReflector {
                 Class<?>[] paramTypes = method.getParameterTypes();
                 Parameter[] params = method.getParameters();
                 int N = method.getParameterCount();
-                // Annotation[][] params = method.getParameterAnnotations();
                 for (int i = 0; i < N; i++) {
-                    toPrint += paramTypes[i].getName() + " " + params[i].getName() + ", ";
+                    toPrint += paramTypes[i].getName() + " " + params[i].getName();
+                    if (i < N - 1) {
+                        toPrint += ", ";
+                    }
                 }
                 toPrint += ") returns " + method.getReturnType().getName();
                 System.out.println(toPrint);
