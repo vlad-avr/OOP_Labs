f6d7214e383ed28b5d275131e20a005e89345bc3
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-11-07 15:38:34 +0200

    Implemented and tested barrier

--- Tasks/demo/src/main/java/com/example/App.java
+++ Tasks/demo/src/main/java/com/example/App.java
@@ -1,12 +1,43 @@
 package com.example;
 
+import java.security.SecureRandom;
+import java.util.concurrent.BrokenBarrierException;
+
+import com.example.CyclicBarrier.CyclicBarrier;
 import com.example.ReflectionTask4.MyReflector;
 
 public final class App {
     private App() {
     }
+
     public static void main(String[] args) {
-        MyReflector reflector = new MyReflector();
-        reflector.reflectClass(Thread.class);
+        // MyReflector reflector = new MyReflector();
+        // reflector.reflectClass(Thread.class);
+        CyclicBarrier myBarrier = new CyclicBarrier(6, () -> {
+            System.out.println("\nBarrier has been reached by all threads!\n");
+        });
+        SecureRandom rnd = new SecureRandom();
+        for (int j = 0; j < 5; j++) {
+            for (int i = 0; i < myBarrier.getParties()-1; i++) {
+                Thread thr = new Thread(() -> {
+                    try {
+                        int t = rnd.nextInt(5000);
+                        System.out.println("\nThread " + Thread.currentThread().getName() + " will do something for "
+                                + t + " milliseconds");
+                        Thread.sleep(t);
+                        System.out.println("\nThread " + Thread.currentThread().getName() + " has finished its work");
+                        myBarrier.await();
+                    } catch (InterruptedException | BrokenBarrierException e) {
+                        System.out.println(e.getMessage());
+                    }
+                });
+                thr.start();
+            }
+            try {
+                myBarrier.await();
+            } catch (BrokenBarrierException | InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
--- Tasks/demo/src/main/java/com/example/CyclicBarrier/CyclicBarrier.java
+++ Tasks/demo/src/main/java/com/example/CyclicBarrier/CyclicBarrier.java
@@ -1,16 +1,65 @@
 package com.example.CyclicBarrier;
 
+import java.util.concurrent.BrokenBarrierException;
+
 public class CyclicBarrier {
     private final int parties;
     private int count = 0;
+    private boolean broken = false;
     private Runnable action;
 
     public CyclicBarrier(int parties){
+        assert(parties > 0);
         this.parties = parties;
     }
 
     public CyclicBarrier(int parties, Runnable action){
+        assert(parties > 0);
         this.parties = parties;
         this.action = action;
     }
+
+    public synchronized void await() throws BrokenBarrierException, InterruptedException{
+        if(broken){
+            throw new BrokenBarrierException();
+        }
+        int ind = count;
+        count++;
+        if(count == parties){
+            count = 0;
+            notifyAll();
+            if (action != null) {
+                action.run();
+            }
+        }else{
+            while (ind == count) {
+                wait();
+            }
+        }
+    }
+
+    public synchronized void breakBarrier(){
+        broken = true;
+        count = 0;
+        notifyAll();
+    }
+
+    public synchronized boolean isBroken(){
+        return this.broken;
+    }
+
+    public synchronized void reset(){
+        count = 0;
+        broken = false;
+    }
+
+    public synchronized void resetBroken(){
+        count = 0;
+        broken = false;
+        notifyAll();
+    }
+
+    public int getParties(){
+        return this.parties;
+    } 
 }
