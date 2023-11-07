027c2300d137e69beab62d034879f776d8f03f74
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-11-07 17:10:35 +0200

    implemented custom thread pool - requires fix

--- Tasks/demo/src/main/java/com/example/App.java
+++ Tasks/demo/src/main/java/com/example/App.java
@@ -5,6 +5,7 @@ import java.util.concurrent.BrokenBarrierException;
 
 import com.example.CyclicBarrier.CyclicBarrier;
 import com.example.ReflectionTask4.MyReflector;
+import com.example.ThreadPool.MyThreadPool;
 
 public final class App {
     private App() {
@@ -13,33 +14,50 @@ public final class App {
     public static void main(String[] args) {
         // MyReflector reflector = new MyReflector();
         // reflector.reflectClass(Thread.class);
-        CyclicBarrier myBarrier = new CyclicBarrier(6, () -> {
-            System.out.println("\nBarrier has been reached by all threads!\n");
-        });
+        // CyclicBarrier myBarrier = new CyclicBarrier(6, () -> {
+        //     System.out.println("\nBarrier has been reached by all threads!\n");
+        // });
+        // SecureRandom rnd = new SecureRandom();
+        // for (int j = 0; j < 3; j++) {
+        //     System.out.println("\n Barrier reset - starting all over again \n");
+        //     for (int i = 0; i < myBarrier.getParties() - 1; i++) {
+        //         Thread thr = new Thread(() -> {
+        //             try {
+        //                 int t = rnd.nextInt(5000) + 1000;
+        //                 System.out.println("\nThread " + Thread.currentThread().getName() + " will do something for "
+        //                         + t + " milliseconds");
+        //                 Thread.sleep(t);
+        //                 System.out.println("\nThread " + Thread.currentThread().getName() + " has finished its work");
+        //                 myBarrier.await();
+        //                 System.out.println("\nThread " + Thread.currentThread().getName() + " has breached the containment");
+        //             } catch (InterruptedException | BrokenBarrierException e) {
+        //                 System.out.println(e.getMessage());
+        //             }
+        //         });
+        //         thr.start();
+        //     }
+        //     try {
+        //         myBarrier.await();
+        //     } catch (BrokenBarrierException | InterruptedException e) {
+        //         System.out.println(e.getMessage());
+        //     }
+        // }
+        MyThreadPool myPool = new MyThreadPool(4);
         SecureRandom rnd = new SecureRandom();
-        for (int j = 0; j < 3; j++) {
-            System.out.println("\n Barrier reset - starting all over again \n");
-            for (int i = 0; i < myBarrier.getParties() - 1; i++) {
-                Thread thr = new Thread(() -> {
-                    try {
-                        int t = rnd.nextInt(5000) + 1000;
-                        System.out.println("\nThread " + Thread.currentThread().getName() + " will do something for "
-                                + t + " milliseconds");
-                        Thread.sleep(t);
-                        System.out.println("\nThread " + Thread.currentThread().getName() + " has finished its work");
-                        myBarrier.await();
-                        System.out.println("\nThread " + Thread.currentThread().getName() + " has breached the containment");
-                    } catch (InterruptedException | BrokenBarrierException e) {
-                        System.out.println(e.getMessage());
-                    }
-                });
-                thr.start();
-            }
-            try {
-                myBarrier.await();
-            } catch (BrokenBarrierException | InterruptedException e) {
-                System.out.println(e.getMessage());
-            }
+        for(int i = 0; i < 10; i++){
+            int counter = i+1;
+            myPool.submit(() -> {
+                // int t = rnd.nextInt(2000) + 1000;
+                // System.out.println(Thread.currentThread().getName() + " will be working on Task " + counter + " for " + t + " milliseconds.");
+                // try {
+                //     Thread.sleep(t);
+                // } catch (InterruptedException e) {
+                //     System.out.println(e.getMessage());
+                // }
+                // System.out.println(Thread.currentThread().getName() + " has finished working on Task " + counter);
+                System.out.println("Task " + counter + " executed by thread: " + Thread.currentThread().getName());
+            });
         }
+        myPool.shutdown();
     }
 }
--- Tasks/demo/src/main/java/com/example/ThreadPool/MyThreadPool.java
+++ Tasks/demo/src/main/java/com/example/ThreadPool/MyThreadPool.java
@@ -1,7 +1,44 @@
 package com.example.ThreadPool;
 
-
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class MyThreadPool {
-    
+    private final WorkerThread[] workers;
+    private final BlockingQueue<Runnable> taskQueue = new LinkedBlockingQueue<>();
+
+    public MyThreadPool(int poolSize) {
+        this.workers = new WorkerThread[poolSize];
+        for (int i = 0; i < poolSize; i++) {
+            workers[i] = new WorkerThread();
+            workers[i].start();
+        }
+    }
+
+    public void submit(Runnable task) {
+        try {
+            taskQueue.put(task);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public void shutdown() {
+        for (WorkerThread worker : workers) {
+            worker.interrupt();
+        }
+    }
+
+    private class WorkerThread extends Thread {
+        public void run() {
+            while (!isInterrupted()) {
+                try {
+                    Runnable task = taskQueue.take();
+                    task.run();
+                } catch (InterruptedException e) {
+                    interrupt();
+                }
+            }
+        }
+    }
 }
