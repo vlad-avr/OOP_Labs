04463abef76e58309af444d4834a25ae99a6823b
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-11-22 17:50:53 +0200

    reverted changes - will go with different approach

--- OOP_lab3/app/src/main/java/com/example/oop_lab3/gamepanel/MoveJoystick.java
+++
@@ -1,7 +0,0 @@
-package com.example.oop_lab3.gamepanel;
-
-public class MoveJoystick extends Joystick{
-    public MoveJoystick(int centerPositionX, int centerPositionY, int outerCircleRadius, int innerCircleRadius) {
-        super(centerPositionX, centerPositionY, outerCircleRadius, innerCircleRadius);
-    }
-}
--- OOP_lab3/app/src/main/java/com/example/oop_lab3/gamepanel/ShootJoystick.java
+++
@@ -1,7 +0,0 @@
-package com.example.oop_lab3.gamepanel;
-
-public class ShootJoystick extends Joystick{
-    public ShootJoystick(int centerPositionX, int centerPositionY, int outerCircleRadius, int innerCircleRadius) {
-        super(centerPositionX, centerPositionY, outerCircleRadius, innerCircleRadius);
-    }
-}
