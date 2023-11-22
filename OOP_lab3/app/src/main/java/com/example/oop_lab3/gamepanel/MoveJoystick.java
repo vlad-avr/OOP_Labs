382d8682bb74eb57d997567422fdd09c3136eab0
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-11-22 17:03:39 +0200

    started work on controls to shoot/move

---
+++ OOP_lab3/app/src/main/java/com/example/oop_lab3/gamepanel/MoveJoystick.java
@@ -0,0 +1,7 @@
+package com.example.oop_lab3.gamepanel;
+
+public class MoveJoystick extends Joystick{
+    public MoveJoystick(int centerPositionX, int centerPositionY, int outerCircleRadius, int innerCircleRadius) {
+        super(centerPositionX, centerPositionY, outerCircleRadius, innerCircleRadius);
+    }
+}
---
+++ OOP_lab3/app/src/main/java/com/example/oop_lab3/gamepanel/ShootJoystick.java
@@ -0,0 +1,7 @@
+package com.example.oop_lab3.gamepanel;
+
+public class ShootJoystick extends Joystick{
+    public ShootJoystick(int centerPositionX, int centerPositionY, int outerCircleRadius, int innerCircleRadius) {
+        super(centerPositionX, centerPositionY, outerCircleRadius, innerCircleRadius);
+    }
+}
