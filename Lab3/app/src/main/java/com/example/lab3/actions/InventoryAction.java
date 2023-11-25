fd3d3802eff8dd49b77ab9bfca9d48b8c0e95fd4
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-11-25 14:22:42 +0200

    continued work on inventory system

---
+++ Lab3/app/src/main/java/com/example/lab3/actions/InventoryAction.java
@@ -0,0 +1,4 @@
+package com.example.lab3.actions;
+
+public class InventoryAction extends Action{
+}
--- Lab3/app/src/main/java/com/example/lab3/inventory/Item.java
+++ Lab3/app/src/main/java/com/example/lab3/inventory/Item.java
@@ -1,13 +1,16 @@
 package com.example.lab3.inventory;
 
+import com.example.lab3.actions.InventoryAction;
 import com.example.lab3.graphics.Sprite;
 
-public class Item {
+public abstract class Item {
     private Sprite image;
     private String desc;
 
+    private InventoryAction action;
     public Item(Sprite image, String desc){
         this.image = image;
         this.desc = desc;
     }
+
 }
